5bc0514719fddee3360405af088dbc51eb77b880
stephantul <stephan.tulkens@uantwerpen.be>
2017-04-11 10:51:28 +0200

    Added batching
    Batching is not standard right now, since it doesn't work for recursive soms for batch sizes > 1. If this issue is resolved, batching will become standard. If you use the regular SOM, batching is faster.

---
+++ somber/batch/recursive.py
@@ -0,0 +1,157 @@
+import numpy as np
+import logging
+
+from somber.batch.som import Som
+from somber.utils import expo, progressbar
+
+
+logger = logging.getLogger(__name__)
+
+
+class Recursive(Som):
+
+    def __init__(self, map_dim, dim, learning_rate, alpha, beta, sigma=None, lrfunc=expo, nbfunc=expo):
+
+        super().__init__(map_dim, dim, learning_rate, lrfunc, nbfunc, sigma, min_max=np.argmax)
+        self.context_weights = np.zeros((self.map_dim, self.map_dim))
+        self.alpha = alpha
+        self.beta = beta
+
+    def _train_loop(self, X, num_epochs, lr_update_counter, nb_update_counter, context_mask, show_progressbar):
+        """
+        The train loop. Is a separate function to accomodate easy inheritance.
+
+        :param X: The input data.
+        :param lr_update_counter: A list of indices at which the params need to be updated.
+        :return: None
+        """
+
+        nb_step = 0
+        lr_step = 0
+
+        # Calculate the influences for update 0.
+        map_radius = self.nbfunc(self.sigma, 0, len(nb_update_counter))
+        learning_rate = self.lrfunc(self.learning_rate, 0, len(nb_update_counter))
+        influences = self._calculate_influence(map_radius)
+        update = False
+
+        idx = 0
+
+        for epoch in range(num_epochs):
+
+            prev_activation = np.zeros((X.shape[1], self.map_dim))
+
+            for x in progressbar(X, mult=X.shape[1], use=show_progressbar):
+
+                prev_activation = self._example(x, influences, prev_activation=prev_activation)
+
+                if idx in nb_update_counter:
+                    nb_step += 1
+
+                    map_radius = self.nbfunc(self.sigma, nb_step, len(nb_update_counter))
+                    logger.info("Updated map radius: {0}".format(map_radius))
+                    update = True
+
+                if idx in lr_update_counter:
+
+                    lr_step += 1
+
+                    learning_rate = self.lrfunc(self.learning_rate, lr_step, len(lr_update_counter))
+                    logger.info("Updated learning rate: {0}".format(learning_rate))
+                    update = True
+
+                if update:
+
+                    influences = self._calculate_influence(map_radius) * learning_rate
+                    update = False
+
+                idx += 1
+
+    def _example(self, x, influences, **kwargs):
+        """
+        A single epoch.
+        :param X: a numpy array of data
+        :param map_radius: The radius at the current epoch, given the learning rate and map size
+        :param learning_rates: The learning rate.
+        :param batch_size: The batch size
+        :return: The best matching unit
+        """
+
+        prev_activation = kwargs['prev_activation']
+
+        activation, diff_x, diff_context = self._get_bmus(x, prev_activation=prev_activation)
+
+        influence, bmu = self._apply_influences(activation, influences)
+
+        # Update
+        self.weights += self._calculate_update(diff_x, influence).mean(axis=0)
+        # print(influence.shape)
+        self.context_weights += self._calculate_update(diff_context, influence).mean(axis=0)
+
+        return activation
+
+    def _create_batches(self, X, batch_size):
+        """
+        Creates batches out of a sequential piece of data.
+        Assumes ndim(X) == 2.
+
+        This function will append zeros to the end of your data to make all batches even-sized.
+
+        For the recursive SOM, this function does not simply resize your data. It will create
+        subsequences.
+
+        :param X: A numpy array, representing your input data. Must have 2 dimensions.
+        :param batch_size: The desired batch size.
+        :return: A batched version of your data.
+        """
+
+        # This line first resizes the data to (batch_size, len(X) / batch_size, data_dim)
+        X = np.resize(X, (batch_size, int(np.ceil(X.shape[0] / batch_size)), X.shape[1]))
+        # Transposes it to (len(X) / batch_size, batch_size, data_dim)
+        return X.transpose((1, 0, 2))
+
+    def _get_bmus(self, x, **kwargs):
+        """
+        Gets the best matching units, based on euclidean distance.
+        :param x: The input vector
+        :return: An integer, representing the index of the best matching unit.
+        """
+
+        prev_activation = kwargs['prev_activation']
+
+        # Differences is the components of the weights subtracted from the weight vector.
+        difference_x = self._distance_difference(x, self.weights)
+        difference_y = self._distance_difference(prev_activation, self.context_weights)
+
+        # Distances are squared euclidean norm of differences.
+        # Since euclidean norm is sqrt(sum(square(x)))) we can leave out the sqrt
+        # and avoid doing an extra square.
+        distance_x = self._euclidean(x, self.weights)
+        distance_y = self._euclidean(prev_activation, self.context_weights)
+
+        activation = np.exp(-(self.alpha * distance_x + self.beta * distance_y))
+
+        return activation, difference_x, difference_y
+
+    def _predict_base(self, X):
+        """
+        Predicts distances to some input data.
+
+        :param X: The input data.
+        :return: An array of arrays, representing the activation
+        each node has to each input.
+        """
+
+        X = self._create_batches(X, 1)
+
+        # Return the indices of the BMU which matches the input data most
+        distances = []
+
+        prev_activation = np.sum(np.square(self._distance_difference(X[0], self.weights)), axis=2)
+        distances.extend(prev_activation)
+
+        for x in X[1:]:
+            prev_activation, _, _ = self._get_bmus(x, prev_activation=prev_activation)
+            distances.extend(prev_activation)
+
+        return np.array(distances)
---
+++ somber/batch/som.py
@@ -0,0 +1,188 @@
+import logging
+import time
+import numpy as np
+
+from somber.utils import progressbar, expo, linear, static
+from somber.som import Som as Base_Som
+
+
+logger = logging.getLogger(__name__)
+
+
+class Som(Base_Som):
+    """
+    This is the basic SOM class.
+    """
+
+    def __init__(self,
+                 map_dim,
+                 dim,
+                 learning_rate,
+                 lrfunc=expo,
+                 nbfunc=expo,
+                 sigma=None,
+                 min_max=np.argmin):
+        """
+
+        :param map_dim: A tuple of map dimensions, e.g. (10, 10) instantiates a 10 by 10 map.
+        :param dim: The data dimensionality.
+        :param learning_rate: The learning rate, which is decreases according to some function
+        :param lrfunc: The function to use in decreasing the learning rate. The functions are
+        defined in utils. Default is exponential.
+        :param nbfunc: The function to use in decreasing the neighborhood size. The functions
+        are defined in utils. Default is exponential.
+        :param sigma: The starting value for the neighborhood size, which is decreased over time.
+        If sigma is None (default), sigma is calculated as ((max(map_dim) / 2) + 0.01), which is
+        generally a good value.
+        """
+
+        super().__init__(map_dim=map_dim,
+                         dim=dim,
+                         learning_rate=learning_rate,
+                         lrfunc=lrfunc,
+                         nbfunc=nbfunc,
+                         sigma=sigma,
+                         min_max=min_max)
+
+    def train(self, X, num_epochs=10, total_updates=10, stop_lr_updates=1.0, stop_nb_updates=1.0, context_mask=(), batch_size=100, show_progressbar=False):
+        """
+        Fits the SOM to some data.
+        The updates correspond to the number of updates to the parameters
+        (i.e. learning rate, neighborhood, not weights!) to perform during training.
+
+        In general, 1000 updates will do for most learning problems.
+
+        :param X: the data on which to train.
+        :param total_updates: The number of updates to the parameters to do during training.
+        :param stop_lr_updates: A fraction, describing over which portion of the training data
+        the neighborhood and learning rate should decrease. If the total number of updates, for example
+        is 1000, and stop_updates = 0.5, 1000 updates will have occurred after half of the examples.
+        After this period, no updates of the parameters will occur.
+        :param context_mask: a binary mask used to indicate whether the context should be set to 0
+        at that specified point in time. Used to make items conditionally independent on previous items.
+        Examples: Spaces in character-based models of language. Periods and question marks in models of sentences.
+        :return: None
+        """
+
+        # The train length
+        train_length = (len(X) * num_epochs) // batch_size
+
+        X = self._create_batches(X, batch_size)
+
+        # The step size is the number of items between rough epochs.
+        # We use len instead of shape because len also works with np.flatiter
+        step_size_lr = max((train_length * stop_lr_updates) // total_updates, 1)
+        step_size_nb = max((train_length * stop_nb_updates) // total_updates, 1)
+
+        # Precalculate the number of updates.
+        lr_update_counter = np.arange(step_size_lr, (train_length * stop_lr_updates) + step_size_lr, step_size_lr)
+        nb_update_counter = np.arange(step_size_nb, (train_length * stop_nb_updates) + step_size_nb, step_size_nb)
+
+        start = time.time()
+
+        # Train
+        self._train_loop(X, num_epochs, lr_update_counter, nb_update_counter, context_mask, show_progressbar)
+        self.trained = True
+
+        logger.info("Total train time: {0}".format(time.time() - start))
+
+    def _create_batches(self, X, batch_size):
+        """
+        Creates batches out of a sequential piece of data.
+        Assumes ndim(X) == 2.
+
+        This function will append zeros to the end of your data to make all batches even-sized.
+
+        :param X: A numpy array, representing your input data. Must have 2 dimensions.
+        :param batch_size: The desired batch size.
+        :return: A batched version of your data.
+        """
+        return np.resize(X, (int(np.ceil(X.shape[0] / batch_size)), batch_size, X.shape[1]))
+
+    def _example(self, x, influences, **kwargs):
+        """
+        A single example.
+
+        :param x: a single example
+        :param influences: an array with influence values.
+        :return: The best matching unit
+        """
+
+        # activation, difference_x = self._get_bmus(x)
+        activation, difference_x = self._get_bmus(x)
+
+        influences, bmu = self._apply_influences(activation, influences)
+        self.weights += self._calculate_update(difference_x, influences).mean(axis=0)
+
+        return bmu
+
+    def _get_bmus(self, x):
+        """
+        Gets the best matching units, based on euclidean distance.
+
+        :param x: The input vector
+        :return: The activations, which is a vector of map_dim, and
+         the distances between the input and the weights, which can be
+         reused in the update calculation.
+        """
+
+        diff = self._distance_difference(x, self.weights)
+        euclidean = self._euclidean(x, self.weights)
+
+        return euclidean, diff
+
+    def _euclidean(self, x, weights):
+
+        m_norm = np.square(x).sum(axis=1)
+        w_norm = np.square(weights).sum(axis=1)[:, np.newaxis]
+        dotted = np.dot(np.multiply(x, 2), weights.T)
+
+        res = np.outer(m_norm, np.ones((1, w_norm.shape[0])))
+        res += np.outer(np.ones((m_norm.shape[0], 1)), w_norm.T)
+        res -= dotted
+
+        return res
+
+    def _distance_difference(self, x, weights):
+        """
+        Calculates the difference between an input and all the weights.
+
+        :param x: The input.
+        :param weights: An array of weights.
+        :return: A vector of differences.
+        """
+
+        return np.array([v - weights for v in x])
+
+    def _predict_base(self, X):
+        """
+        Predicts distances to some input data.
+
+        :param X: The input data.
+        :return: An array of arrays, representing the activation
+        each node has to each input.
+        """
+
+        X = self._create_batches(X, 1)
+
+        distances = []
+
+        for x in X:
+            distance, _ = self._get_bmus(x)
+            distances.extend(distance)
+
+        return np.array(distances)
+
+    def _apply_influences(self, activations, influences):
+        """
+        First calculates the BMU.
+        Then gets the appropriate influence from the neighborhood, given the BMU
+
+        :param activations: A Numpy array of distances.
+        :param influences: A (map_dim, map_dim, data_dim) array describing the influence
+        each node has on each other node.
+        :return: The influence given the bmu, and the index of the bmu itself.
+        """
+
+        bmu = self.min_max(activations, axis=1)
+        return influences[bmu], bmu
