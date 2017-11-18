794cf36432d0c6b8269eabf371c154f3094ef91f
stephantul <stephan.tulkens@uantwerpen.be>
2017-11-18 23:31:19 +0000

    update setup, getting ready for 2.0

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ from setuptools import find_packages
 
 
 setup(name='somber',
-      version='1.1.0',
+      version='2.0.0',
       description='Self-Organizing Maps in Numpy',
       author='Stéphan Tulkens',
       author_email='stephan.tulkens@uantwerpen.be',
       url='https://github.com/stephantul/somber',
       license='MIT',
       packages=find_packages(exclude=['examples']),
-      install_requires=['numpy>=1.11.0', 'cupy>=1.0.0.1'],
+      install_requires=['numpy>=1.11.0'],
       classifiers=[
           'Intended Audience :: Developers',
-
-          'Programming Language :: Python :: 3',],
+          'Programming Language :: Python :: 3'],
       keywords='self-organizing maps machine learning unsupervised',
       zip_safe=True)
