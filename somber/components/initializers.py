e14f97619f8874c9e8b4ad7254eca20bedd83f5e
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-15 12:06:42 +0100

    fixed bug with negative input datums in range initialization

--- somber/components/initializers.py
+++ somber/components/initializers.py
@@ -7,7 +7,7 @@ called to initialize the weight to a value at the beginning of training.
 import numpy as np
 
 
-def range_initialization(X, weights):
+def range_initialization(X, num_weights):
     """
     Initialize the weights by calculating the range of the data.
 
@@ -18,8 +18,8 @@ def range_initialization(X, weights):
     ----------
     X : numpy array
         The input data. The data range is calculated over the last axis.
-    weights : numpy array
-        The weights. These are not modified in the function.
+    num_weights : int
+        The number of weights to initialize.
 
     Returns
     -------
@@ -28,13 +28,10 @@ def range_initialization(X, weights):
         by X.
 
     """
-    datalen = np.prod(X.shape[:-1])
-
     # Randomly initialize weights to cover the range of each feature.
-    X_ = X.reshape(datalen, X.shape[-1])
-    min_val = X_.min(0)
-    max_val = X_.max(0)
+    X_ = X.reshape(-1, X.shape[-1])
+    min_val, max_val = X_.min(0), X_.max(0)
     data_range = max_val - min_val
 
-    return data_range * np.random.rand(len(weights),
-                                       X.shape[-1]) - np.abs(min_val)
+    return data_range * np.random.rand(num_weights,
+                                       X.shape[-1]) + min_val
