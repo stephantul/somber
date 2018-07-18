29bd0bd6b0653d0832c891213275578475df4cd2
stephantul <stephan.tulkens@uantwerpen.be>
2018-07-18 14:20:39 +0200

    Make _check_input return and reshape

--- somber/__init__.py
+++ somber/__init__.py
@@ -3,5 +3,11 @@ from .som import Som
 from .plsom import PLSom
 from .ng import Ng
 from .sequential import RecursiveSom, RecursiveNg
+from .miikkulainen import MiikkulainenSom
 
-__all__ = ['Som', 'Ng', 'RecursiveSom', 'RecursiveNg', 'PLSom']
+__all__ = ['Som',
+           'Ng',
+           'RecursiveSom',
+           'RecursiveNg',
+           'PLSom',
+           'MiikkulainenSom']
