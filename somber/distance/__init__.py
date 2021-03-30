2df96e3e61bd9b68bce99d49ef6985ec7659aa04
Stephan Tulkens <stephan.tulkens@uantwerpen.be>
2021-03-30 08:13:57 +0200

    Make importing euclidean less painful

--- somber/distance/__init__.py
+++ somber/distance/__init__.py
@@ -1,3 +1,7 @@
-from .distance import euclidean
+import pyximport
+import numpy as np
+
+pyximport.install(setup_args={"include_dirs": np.get_include()})
+from .distance import euclidean  # noqa
 
 __all__ = ["euclidean"]
